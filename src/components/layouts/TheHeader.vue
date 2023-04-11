72eff54d9167a15ecba6d4083dad2e910c52e7c2
JiaLe <jialeee0511@gmail.com>
2023-04-11 23:03:02 +0800

    Added Header

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <TheNavigationBar />
+    </header>
+</template>
+
+<script>
+import TheNavigationBar from './TheNavigationBar.vue';
+
+export default {
+    components: {
+        TheNavigationBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
