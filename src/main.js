0e30cd5cc0816da837cb4273b705ed402f038434
JiaLe <jialeee0511@gmail.com>
2023-04-11 22:30:34 +0800

    Replaced pre-stylesheets with own stylesheet

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App)
 
