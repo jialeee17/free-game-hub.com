a047fa6087ad6add913ccb03a729409eefbbce23
bomi <jiale@9loop.asia>
2023-04-13 16:58:44 +0800

    Created Header(Desktop View)

--- src/components/layouts/TheNavigationBar.vue
+++ src/components/layouts/TheNavigationBar.vue
@@ -1,19 +1,22 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light px-3">
+        <router-link to="/" class="navbar-brand">FreeGameHub.com</router-link>
+
+        <!-- Hamburger Menu (Mobile) -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Games</a>
                 </li>
-                <li class="nav-item dropdown">
+                <!-- <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown
                     </a>
@@ -23,15 +26,60 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Something else here</a>
                     </div>
-                </li>
+                </li> -->
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link text-muted" href="#">Recommendations (Comming Soon)</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0 d-flex gap-2">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button class="btn btn-outline-success btn-search my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+    background: rgba(33, 34, 51, 0.9);
+
+    .navbar-brand {
+        color: #6842FF;
+        font-size: 1.5rem;
+        text-shadow: -0.5px -0.5px 0 #ffffff;
+    }
+
+    .navbar-nav {
+        .nav-item {
+            .nav-link {
+                color: #ffffff;
+            }
+        }
+    }
+
+    form {
+
+        input {
+            &[type="search"] {
+                background: rgb(55, 57, 82);
+                border: none;
+                border-radius: 1.875rem;
+            }
+        }
+
+        .btn {
+            &.btn-search {
+                background: #6842FF;
+                border-radius: 1.875rem;
+                color: #ffffff;
+            }
+        }
+    }
+}
+</style>
