971169af58b0f96c5ed03c0654dfd97f366df246
bomi <jiale@9loop.asia>
2023-04-14 14:38:38 +0800

    Updated API Repository

--- src/api/freeToGameRepository.js
+++ src/api/freeToGameRepository.js
@@ -2,10 +2,34 @@ import axios from 'axios';
 
 // API Document: https://www.freetogame.com/api-doc
 
+/* -------------------------------------------------------------------------- */
+/*                                 Rate Limits                                */
+/* -------------------------------------------------------------------------- */
+// Please avoid doing more than 4 requests per second.
+
+/* -------------------------------------------------------------------------- */
+/*                                  Responses                                 */
+/* -------------------------------------------------------------------------- */
+// 200: Success
+// 404: Object not found: Game or endpoint not found
+// 500: Something wrong on our end (unexpected server errors)
+
 const API_URL = 'https://www.freetogame.com/api';
 
 export default {
-    getGameList() {
-        return axios.get(`${API_URL}/games`);
+    getGameList(params) {
+        return axios.get(`${API_URL}/games`, {
+                params: params
+            })
+            .then(response => response)
+            .catch(error => error);
+    },
+
+    filterGameList(params) {
+        return axios.get(`${API_URL}/filter`, {
+                params: params
+            })
+            .then(response => response)
+            .catch(error => error);
     },
 }
